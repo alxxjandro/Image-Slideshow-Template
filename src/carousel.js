e425bb1027a8596b151ce51af7346c6014ae907c
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2025-02-01 12:07:56 -0600

    Setting up the basics

---
+++ src/carousel.js
@@ -0,0 +1,5 @@
+console.log("this is a test");
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
--- webpack.common.js
+++ webpack.common.js
@@ -3,13 +3,25 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: "./src/index.js",
+    app: "./src/carousel.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Production",
+      template: "./src/template.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
