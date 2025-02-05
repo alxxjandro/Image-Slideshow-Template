b6bd3a4ddae2c17385e3d6bdc995165af1fc00a6
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2025-02-05 17:53:14 -0600

    finished the basic functionatilly of the carousel

--- src/carousel.js
+++ src/carousel.js
@@ -1,5 +1,86 @@
-console.log("this is a test");
+import { images } from "./images";
+import "./carousel.css";
 
-for (let i = 0; i < 10; i++) {
-  console.log(i);
-}
+const createSlide = (image, textContent, firstSlide = false) => {
+  const divContainer = Object.assign(document.createElement("div"), {
+    className: "mySlide",
+  });
+  if (!firstSlide) divContainer.classList.add("hidden");
+
+  const slideImage = Object.assign(document.createElement("img"), {
+    src: image,
+    classList: "slideImage",
+  });
+  const slideText = Object.assign(document.createElement("p"), {
+    innerText: textContent,
+  });
+
+  divContainer.appendChild(slideImage);
+  divContainer.appendChild(slideText);
+
+  return divContainer;
+};
+
+const createSlideShow = (slides) => {
+  const slideShowContainer = document.createElement("div");
+
+  slides.forEach((slide) => {
+    slideShowContainer.appendChild(slide);
+  });
+
+  //add the previous and next buttons to the images
+  const previousBtn = Object.assign(document.createElement("a"), {
+    innerText: "❮",
+    classList: "prev",
+  });
+  const nextBtn = Object.assign(document.createElement("a"), {
+    innerText: "❯",
+    classList: "next",
+  });
+
+  nextBtn.addEventListener("click", () => {
+    changeSlide(slides, true);
+  });
+
+  previousBtn.addEventListener("click", () => {
+    changeSlide(slides);
+  });
+
+  slideShowContainer.appendChild(previousBtn);
+  slideShowContainer.appendChild(nextBtn);
+
+  return slideShowContainer;
+};
+
+const changeSlide = (slides, nextSlide = false) => {
+  let currentIndex;
+  //find the current showing photo index
+  slides.forEach((slide, index) => {
+    if (!Array.from(slide.classList).includes("hidden")) currentIndex = index;
+    slide.classList = "";
+    slide.classList.add("mySlide");
+    slide.classList.add("hidden");
+  });
+
+  if (nextSlide) {
+    if (currentIndex == slides.length - 1) currentIndex = -1;
+    slides[currentIndex + 1].classList.remove("hidden");
+  } else {
+    if (currentIndex - 1 == -1) currentIndex = slides.length;
+    slides[currentIndex - 1].classList.remove("hidden");
+  }
+};
+
+let slides = [
+  createSlide(images.img1, "Caption Text", true),
+  createSlide(images.img2, "Caption Text"),
+  createSlide(images.img3, "Caption Text"),
+  createSlide(images.img4, "Caption Text"),
+  createSlide(images.img5, "Caption Text"),
+  createSlide(images.img6, "Caption Text"),
+];
+
+const body = document.body;
+const slideShow = createSlideShow(slides);
+slideShow.classList.add("slideshowContainer");
+body.appendChild(slideShow);
